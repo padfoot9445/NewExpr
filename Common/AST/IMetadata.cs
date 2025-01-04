101375089277fa42154422080fbefe058d5654a9
padfoot9445 <yipy214@gmail.com>
2025-01-04 18:23:17 +0000

    Added Attribute property to ASTNode

---
+++ Common/AST/IMetadata.cs
@@ -0,0 +1,5 @@
+namespace Common.AST;
+public interface IMetadata
+{
+
+}
--- Common/obj/Debug/net8.0/Common.AssemblyInfo.cs
+++ Common/obj/Debug/net8.0/Common.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Common")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+472f8f66393f43cae72904a538763ba0cdb8f318")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e01128f7917b56b45a87289a143279fc1ef16744")]
 [assembly: System.Reflection.AssemblyProductAttribute("Common")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Common")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

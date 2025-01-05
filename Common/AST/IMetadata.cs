59f120e3a70778e441d868e19efdcd5aebb0a0ae
padfoot9445 <yipy214@gmail.com>
2025-01-05 00:17:37 +0000

    Added Merge methods

--- Common/AST/IMetadata.cs
+++ Common/AST/IMetadata.cs
@@ -1,5 +1,15 @@
 namespace Common.AST;
 public interface IMetadata
 {
-
+    /// <summary>
+    /// Merges attributes from other with self. If there is a conflict, throw. It is likely that other must be the same type as self.
+    /// </summary>
+    /// <param name="other"></param>
+    public void Merge(IMetadata other);
+    /// <summary>
+    /// Similar to Merge but does not throw on conflict. By default prioritizes self, but depends on PrioritizeOther.
+    /// </summary>
+    /// <param name="other"></param>
+    /// <param name="PrioritizeOther"> Whether to Prioritize Attributes from the other.</param>
+    public void ForcedMerge(IMetadata other, bool PrioritizeOther = false);
 }

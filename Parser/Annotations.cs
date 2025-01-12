d4fa9214e19ef293db698600a5fa9ff5f9a7668e
padfoot9445 <yipy214@gmail.com>
2025-01-12 21:48:18 +0000

    Implemented all other than unforced merge

--- Parser/Annotations.cs
+++ Parser/Annotations.cs
@@ -14,18 +14,58 @@ public class Annotations : IMetadata
         this.TypeDenotedByIdentifier = TypeDenotedByIdentifier; // as in, in "int x = 0", the "int" node literal would have have this set; also used for custom identifiers but we don't have that
         this.CanBeResolvedToAssignable = CanBeResolvedToAssignable;
     }
-    public uint? TypeCode { get; } = null;
-    public bool IsEmpty { get; } = false;
-    public uint? TypeDenotedByIdentifier { get; } = null;
-    public bool CanBeResolvedToAssignable { get; } = false;
+    public uint? TypeCode = null;
+    public bool IsEmpty = false;
+    public uint? TypeDenotedByIdentifier = null;
+    public bool CanBeResolvedToAssignable = false;
+    void ForceMergeField<T>(ref T Property, T Value, bool PrioritizeOther)
+    {
+        if (PrioritizeOther)
+        {
+            if (Value is null)
+            {
+                return;
+                //do nothing
+            }
+            Property = Value;
+        }
+        else
+        {
+            if (Property is null)
+            {
+                Property = Value;
+            }
+            else
+            {
+                return;
+                //do nothing
+            }
+        }
+    }
     public void ForcedMerge(IMetadata other, bool PrioritizeOther = false)
     {
-        throw new NotImplementedException();
+        if (other is Annotations otherAnnotations)
+        {
+            ForceMergeField(ref TypeCode, otherAnnotations.TypeCode, PrioritizeOther);
+            ForceMergeField(ref IsEmpty, otherAnnotations.IsEmpty, PrioritizeOther);
+            ForceMergeField(ref TypeDenotedByIdentifier, otherAnnotations.TypeDenotedByIdentifier, PrioritizeOther);
+            ForceMergeField(ref CanBeResolvedToAssignable, otherAnnotations.CanBeResolvedToAssignable, PrioritizeOther);
+        }
+        else
+        {
+            throw new InvalidOperationException("Other must be of type Annotations to be merged with Annotations");
+        }
     }
 
     public bool IsEquivalentTo(IMetadata other)
     {
-        throw new NotImplementedException();
+        return (
+            other is Annotations annotations &&
+            TypeCode == annotations.TypeCode &&
+            IsEmpty == annotations.IsEmpty &&
+            TypeDenotedByIdentifier == annotations.TypeDenotedByIdentifier &&
+            CanBeResolvedToAssignable == annotations.CanBeResolvedToAssignable
+        );
     }
 
     public void Merge(IMetadata other)
@@ -34,6 +74,11 @@ public class Annotations : IMetadata
     }
     public Annotations Copy()
     {
-        throw new NotImplementedException();
+        return new(
+            TypeCode: TypeCode,
+            IsEmpty: IsEmpty,
+            TypeDenotedByIdentifier: TypeDenotedByIdentifier,
+            CanBeResolvedToAssignable: CanBeResolvedToAssignable
+        );
     }
 }

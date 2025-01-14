75a5ed9aedefaefa38994d6d3b6b3b8a4863501d
padfoot9445 <yipy214@gmail.com>
2025-01-14 19:31:22 +0000

    Initial

---
+++ MEXP/Parser/Internals/InternalParserBase.cs
@@ -0,0 +1,19 @@
+using Common.AST;
+using Common.Logger;
+using Common.Tokens;
+using MEXP.Parser;
+
+namespace MEXP.Parser.Internals;
+abstract class InternalParserBase
+{
+    public InternalParserBase(Parser p)
+    {
+        this._Parser = p;
+        Log = _Parser.Log;
+    }
+    private protected Parser _Parser { get; private set; }
+    public abstract bool Parse(out AnnotatedNode<Annotations>? Node);
+    private protected IToken? CurrentToken(int offset = 0, bool Inc = false) => _Parser.CurrentToken(offset, Inc);
+    private protected ILogger Log;
+    private protected int Position => _Parser.Position;
+}

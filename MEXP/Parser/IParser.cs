007742cc62e2a69d5a4269c117f1d4e81cebd22e
padfoot9445 <yipy214@gmail.com>
2025-01-14 19:32:24 +0000

    Changed Parse out variable to be nullable

--- MEXP/Parser/IParser.cs
+++ MEXP/Parser/IParser.cs
@@ -8,6 +8,6 @@ public interface IParser
     public static IParser NewParser(IEnumerable<IToken> Tokens, ILogger? Log = null, ITypeProvider? TP = null) => new Parser(Tokens, Log, TP);
     public ILogger Log { get; }
     public ITypeProvider TP { get; }
-    public static bool Parse(IEnumerable<IToken> tokens, out AnnotatedNode<Annotations> Node, ILogger? Log = null, ITypeProvider? TP = null) => NewParser(tokens.ToArray(), Log, TP).Parse(out Node);
-    public bool Parse(out AnnotatedNode<Annotations> Node);
+    public static bool Parse(IEnumerable<IToken> tokens, out AnnotatedNode<Annotations>? Node, ILogger? Log = null, ITypeProvider? TP = null) => NewParser(tokens.ToArray(), Log, TP).Parse(out Node);
+    public bool Parse(out AnnotatedNode<Annotations>? Node);
 }

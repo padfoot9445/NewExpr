20731d37790ca6ca3fe906b2e2b0452b6eb5e3e6
padfoot9445 <yipy214@gmail.com>
2025-01-14 23:11:37 +0000

    Moved to Parse

--- MEXP/Parser/Internals/ExpressionParser.cs
+++ MEXP/Parser/Internals/ExpressionParser.cs
@@ -6,20 +6,16 @@ class ExpressionParser : InternalParserBase
     public ExpressionParser(Parser p) : base(p)
     {
     }
-    public bool Expression(out AnnotatedNode<Annotations>? Node)
-    {
-        if (SafeParse(_Parser.Declaration, out AnnotatedNode<Annotations>? Add, Suppress: false)) //no additional context to add here so we get the context from safeparse
-        {
-            Node = new(Add!.Attributes.Copy(), ASTNode.NonTerminal(Add!, nameof(Expression))); //TypeCode <- Addition.TypeCode
-            return true;
-        }
-        Node = null;
-        return false;
-    }
     private protected override string Name => "Expression";
 
     public override bool Parse(out AnnotatedNode<Annotations>? Node)
     {
-        return Expression(out Node);
+        if (SafeParse(_Parser.Declaration, out AnnotatedNode<Annotations>? Add, Suppress: false)) //no additional context to add here so we get the context from safeparse
+        {
+            Node = new(Add!.Attributes.Copy(), ASTNode.NonTerminal(Add!, Name)); //TypeCode <- Addition.TypeCode
+            return true;
+        }
+        Node = null;
+        return false;
     }
 }

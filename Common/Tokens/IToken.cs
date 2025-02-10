264a4514f2d0592fa109c169a8d4e2e0aefad138
padfoot9445 <yipy214@gmail.com>
2025-02-10 21:40:13 +0000

    Added Literal and Positional properties to tokens

--- Common/Tokens/IToken.cs
+++ Common/Tokens/IToken.cs
@@ -4,5 +4,7 @@ public interface IToken : IValidASTLeaf
 {
     public TokenType TT { get; }
     public string Lexeme { get; }
-    public static IToken NewToken(TokenType type, string Lexeme, int position) => Token.NewToken(type, Lexeme);
+    public string Literal { get; }
+    public int Position { get; }
+    public static IToken NewToken(TokenType type, string Lexeme, int position, string? literal = null) => Token.NewToken(type, Lexeme, position, literal);
 }
--- Common/Tokens/Token.cs
+++ Common/Tokens/Token.cs
@@ -6,12 +6,13 @@ record class Token : IToken
     public required TokenType TT { get; init; }
 
     public required string Lexeme { get; init; }
-
+    public required string Literal { get; init; }
+    public required int Position { get; init; }
     public ASTLeafType Type => ASTLeafType.Terminal;
 
-    public static Token NewToken(TokenType type, string lexeme)
+    public static Token NewToken(TokenType type, string lexeme, int Position, string? literal = null)
     {
-        return new Token() { TT = type, Lexeme = lexeme };
+        return new Token() { TT = type, Lexeme = lexeme, Position = Position, Literal = literal ?? lexeme };
     }
 
     public ICollection<IValidASTLeaf> Flatten()

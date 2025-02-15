1aac72acca8d95513add20234802f99f7c43a87b
padfoot9445 <yipy214@gmail.com>
2025-02-15 12:36:51 +0000

    ParserData in Common

---
+++ Common/Parser/ParserData.cs
@@ -0,0 +1,49 @@
+using Common.Logger;
+using Common.Tokens;
+
+namespace Common.Parser;
+public class ParserData(int start, int current, List<IToken> tokens, ILogger logger)
+{
+    public int Start => start;
+    public int Current => current;
+    public List<IToken> Tokens => tokens.Select(x => x).ToList();
+    public ILogger Logger => logger;
+    public IToken CurrentToken => GetToken();
+    public IToken? SafeCurrentToken => SafeGetToken();
+    public void BringStart()
+    {
+        start = current;
+    }
+    public IToken Advance(int Inc = 1)
+    {
+        current += Inc;
+        return GetToken(-1);
+    }
+    public IToken Peek(int Inc = 1) => GetToken(Inc);
+    public IToken GetToken(int rel = 0)
+    {
+        return tokens[Current + rel];
+    }
+    public IToken? SafeGetToken(int rel = 0)
+    {
+        if (Current + rel < tokens.Count)
+        {
+            return tokens[Current + rel];
+        }
+        return null;
+    }
+
+    public bool TCmp(params TokenType[] TTs) => TCmp(x => x.TT, (x, y) => x == y, TTs);
+    public bool TCmp(Func<TokenType, TokenType, bool> Predicate, params TokenType[] TTs) => TCmp(x => x.TT, Predicate, TTs);
+    public bool TCmp<T>(Func<IToken, T> Accessor, Func<T, T, bool> Comparer, params T[] TTs)
+    {
+        for (int i = 0; i < TTs.Length; i++)
+        {
+            if (!Comparer(Accessor(GetToken(i)), TTs[i]))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}

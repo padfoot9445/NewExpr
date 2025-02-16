c9139eeeee3ef639c94f491dbcf3923b43fb44e6
padfoot9445 <yipy214@gmail.com>
2025-02-16 16:55:11 +0000

    BaseTypeParser

--- SmallLang/Parser/InternalParsers/BaseTypeParser.cs
+++ SmallLang/Parser/InternalParsers/BaseTypeParser.cs
@@ -7,6 +7,12 @@ class BaseTypeParser(SmallLangParserData data) : BaseInternalParser(data)
 {
     public override bool Parse(out DynamicASTNode<ASTNodeType, Attributes>? Node)
     {
-        throw new NotImplementedException();
+        if (Data.TryConsumeToken(out var TypeToken, TokenType.TypeByte, TokenType.TypeShort, TokenType.TypeInt, TokenType.TypeLong, TokenType.TypeLongInt, TokenType.TypeFloat, TokenType.TypeDouble, TokenType.TypeRational, TokenType.TypeNumber, TokenType.TypeString, TokenType.TypeChar, TokenType.TypeVoid))
+        {
+            Node = new(TypeToken!, [], ASTNodeType.BaseType);
+            return true;
+        }
+        Node = null;
+        return false;
     }
 }

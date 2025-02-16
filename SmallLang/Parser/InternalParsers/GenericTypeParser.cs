f0f904dc8a639b0c57503c984d0fcb81eaeccd10
padfoot9445 <yipy214@gmail.com>
2025-02-16 16:57:32 +0000

    GenericTypeParser

--- SmallLang/Parser/InternalParsers/GenericTypeParser.cs
+++ SmallLang/Parser/InternalParsers/GenericTypeParser.cs
@@ -7,6 +7,12 @@ class GenericTypeParser(SmallLangParserData data) : BaseInternalParser(data)
 {
     public override bool Parse(out DynamicASTNode<ASTNodeType, Attributes>? Node)
     {
-        throw new NotImplementedException();
+        if (Data.TryConsumeToken(out var GenericTypeToken, TokenType.TypeArray, TokenType.TypeList, TokenType.TypeSet, TokenType.TypeDict))
+        {
+            Node = new(GenericTypeToken!, [], ASTNodeType.GenericType);
+            return true;
+        }
+        Node = null;
+        return false;
     }
 }

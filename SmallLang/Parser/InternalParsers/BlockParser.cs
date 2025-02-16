70629b8eca9f48d9d6d17f8ec96b314e7a48f24a
padfoot9445 <yipy214@gmail.com>
2025-02-16 15:24:21 +0000

    Made BlockParser

--- SmallLang/Parser/InternalParsers/BlockParser.cs
+++ SmallLang/Parser/InternalParsers/BlockParser.cs
@@ -1,11 +1,17 @@
 using Common.AST;
 using Common.Parser;
+using Common.Tokens;
 
 namespace SmallLang.Parser.InternalParsers;
 class BlockParser(ParserData data) : BaseInternalParser(data)
 {
     public override bool Parse(out DynamicASTNode<ASTNodeType, Attributes>? Node)
     {
-        throw new NotImplementedException();
+        if (Data.Advance().TT != TokenType.OpenCurly || !SafeParse(Section, out Node) || Data.Advance().TT != TokenType.CloseCurly)
+        {
+            Node = null;
+            return false;
+        }
+        return true;
     }
 }

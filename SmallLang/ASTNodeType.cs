a02eca61f381025cf2f316b79b0a7e78284c0d64
padfoot9445 <yipy214@gmail.com>
2025-02-17 23:12:06 +0000

    Removed cond; action while header

---
+++ SmallLang/ASTNodeType.cs
@@ -0,0 +1,17 @@
+namespace SmallLang;
+enum ASTNodeType
+{
+    Section,
+    Statement,
+    Block,
+    Cond,
+    Expression,
+    Function,
+    LoopCTRL,
+    Loop,
+    OpValInLCTRL,
+    Return,
+    BaseType,
+    GenericType,
+    Type,
+}

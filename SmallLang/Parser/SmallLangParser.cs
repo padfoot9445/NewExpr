974d929329aa0e9e5305b385c1ee25d4bd4e974b
padfoot9445 <yipy214@gmail.com>
2025-02-20 18:37:30 +0000

    Removed dot in function call

--- SmallLang/Parser/SmallLangParser.cs
+++ SmallLang/Parser/SmallLangParser.cs
@@ -185,8 +185,8 @@ public partial class SmallLangParser
     public NodeType NTLPrimary3(LyToken Token) => new(FromToken(Token), [], ASTNodeType.Primary);
     [Production($"{nameof(NTNewExpr)}: New [d] {nameof(NTType)} OpenParen [d] {nameof(NTArgList)}?")]
     public NodeType NTNewExpr(NodeType AType, ValueOption<NodeType> AArgList) => new(null, BuildChildren(AType, AArgList), ASTNodeType.NewExpr);
-    [Production($"{nameof(NTFunctionCallPrime)}: Dot? OpenParen [d] {nameof(NTArgList)} CloseParen [d]")]
-    public NodeType NTFunctionCallPrime(LyToken Dot, NodeType AArgList) => new(FromToken(Dot), BuildChildren(AArgList), ASTNodeType.FunctionCall);
+    [Production($"{nameof(NTFunctionCallPrime)}:OpenParen [d] {nameof(NTArgList)} CloseParen [d]")]
+    public NodeType NTFunctionCallPrime(NodeType AArgList) => new(null, BuildChildren(AArgList), ASTNodeType.FunctionCall);
     [Production($"{nameof(NTArgList)}: {nameof(NTArgListElement)} {nameof(NTArgListPrime)}+")]
     public NodeType NTArgList(NodeType Element, List<NodeType> Elements) => new(null, [Element, .. Elements], ASTNodeType.ArgList);
     [Production($"{nameof(NTArgListElement)}: {nameof(NTArgumentLabel)}? {nameof(NTExpression)}")]

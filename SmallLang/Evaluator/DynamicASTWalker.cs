de465906bebe494c96ac77706d28779de1697679
padfoot9445 <yipy214@gmail.com>
2025-04-14 23:37:47 +0100

    Changed DynamicASTWalker to always require a IDynamicASTVisitor

--- SmallLang/Evaluator/DynamicASTWalker.cs
+++ SmallLang/Evaluator/DynamicASTWalker.cs
@@ -3,21 +3,16 @@ using Common.AST;
 namespace SmallLang.Evaluator;
 static class DynamicASTWalker
 {
-    public static DynamicASTNode<T1, T2> Walk<T1, T2>(this DynamicASTNode<T1, T2> node, Action<DynamicASTNode<T1, T2>?, DynamicASTNode<T1, T2>> action) where T2 : IMetadata
+    public static void Walk<T1, T2>(this DynamicASTNode<T1, T2> node, IDynamicASTVisitor visitor) where T2 : IMetadata
     {
-        return Walk(node, action);
+        Walk(node, visitor, null);
     }
-    static DynamicASTNode<T1, T2> Walk<T1, T2>(DynamicASTNode<T1, T2> node, Action<DynamicASTNode<T1, T2>?, DynamicASTNode<T1, T2>> action, DynamicASTNode<T1, T2>? parent = null) where T2 : IMetadata
+    static void Walk<T1, T2>(DynamicASTNode<T1, T2> node, IDynamicASTVisitor visitor, DynamicASTNode<T1, T2>? parent = null) where T2 : IMetadata
     {
-        action(parent, node);
+        visitor.Dispatch(node)(parent, node);
         foreach (var child in node.Children)
         {
-            Walk(child, action, node);
+            Walk(child, visitor, node);
         }
-        return node;
-    }
-    public static DynamicASTNode<T1, T2> Walk<T1, T2>(this DynamicASTNode<T1, T2> node, IDynamicASTVisitor visitor) where T2 : IMetadata
-    {
-        return Walk(node, visitor.Dispatch(node));
     }
 }

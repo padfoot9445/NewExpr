ca2b2aa46aa08bf5a5c43f77034e830932ccde4b
padfoot9445 <yipy214@gmail.com>
2025-05-05 20:05:27 +0100

    ICall and SCall

--- SmallLang/LinearIR/Opcode.cs
+++ SmallLang/LinearIR/Opcode.cs
@@ -1,5 +1,6 @@
 namespace SmallLang.LinearIR;
 public enum Opcode
 {
-
+    ICall, //ICall FunctionID #arguments are in the stack, Function ID immediate. Stack: [Arg1, Arg2...]
+    SCall, //SCall #Stack: [FunctionID, Arg1, Arg2...]
 }

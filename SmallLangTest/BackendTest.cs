be4cdc3daafee77839c671a3bc4aadf9e34b749c
padfoot9445 <yipy214@gmail.com>
2025-05-06 00:23:08 +0100

    BackendTest with 'input()' passed

---
+++ SmallLangTest/BackendTest.cs
@@ -0,0 +1,17 @@
+using System.Reflection.Emit;
+using Common.LinearIR;
+using SmallLang;
+using SmallLang.LinearIR;
+
+namespace SmallLangTest;
+[TestFixture]
+public class SmallLangTest
+{
+    [Test]
+    public void InputFunctionCall__NoArgs__GeneratesCorrectResult()
+    {
+        var res = HighToLowLevelCompilerDriver.Compile(HTLDriverTest.Program1);
+        Assert.That(res[0].Op.Value, Is.EqualTo((uint)(Opcode.ICall)));
+        Assert.That(res[0].Operands[0].Value, Is.EqualTo(1));
+    }
+}

cc2bc79efe4cb2637d33b709541e682921f6bcb5
padfoot9445 <yipy214@gmail.com>
2025-05-09 22:40:39 +0100

    Added DI for codegen driver

--- SmallLang/HighToLowLevelCompilerDriver.cs
+++ SmallLang/HighToLowLevelCompilerDriver.cs
@@ -5,15 +5,16 @@ using SmallLang.Backend;
 namespace SmallLang;
 public class HighToLowLevelCompilerDriver
 {
-    public static Operation<uint>[] Compile(string Code)
+    public static Operation<uint>[] Compile(string Code, Func<CodeGenVisitor>? GetCodeGenVisitor = null)
     {
+        GetCodeGenVisitor ??= () => new CodeGenVisitor();
         var Ast = new Parser.Parser(Code).Parse();
         var Evaluator = new DynamicASTEvaluator();
         var AttributeAnalyser = new AttributeVisitor();
         var Optimiser = new OptimisingVisitor();
         Evaluator.Evaluate(Ast, AttributeAnalyser);
         Evaluator.Evaluate(Ast, Optimiser);
-        var Generator = new CodeGenVisitor();
+        var Generator = GetCodeGenVisitor();
         Generator.Dispatch(Ast)(null, Ast);
         return Generator.Instructions.ToArray();
     }

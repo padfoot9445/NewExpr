b589f8cb4cd1bd862a001a5afb6c127ee6ee05f8
padfoot9445 <yipy214@gmail.com>
2025-06-22 22:58:34 +0100

    Made non-abstract and added uint -> wrapper explicit conversion

--- SmallLang/BaseUIntWrapper.cs
+++ SmallLang/BaseUIntWrapper.cs
@@ -1,6 +1,7 @@
 namespace SmallLang;
 
-public abstract record class BaseUIntWrapper(uint Value)
+public record class BaseUIntWrapper(uint Value)
 {
     public static implicit operator uint(BaseUIntWrapper value) => value.Value;
+    public static explicit operator BaseUIntWrapper(uint value) => new(value);
 }

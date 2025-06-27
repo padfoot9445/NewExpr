b9ac227befa783875e0ce2e52a37cda5411b6fd3
padfoot9445 <yipy214@gmail.com>
2025-06-27 22:19:24 +0100

    Made Operation and OpcodeWrapper .ToString()

--- Common/LinearIR/Operation.cs
+++ Common/LinearIR/Operation.cs
@@ -1,4 +1,11 @@
 using System.Numerics;
 
 namespace Common.LinearIR;
-public readonly record struct Operation<TBacking>(IOperationArgument<TBacking> Op, params IOperationArgument<TBacking>[] Operands) where TBacking : INumber<TBacking>;
+
+public readonly record struct Operation<TBacking>(IOperationArgument<TBacking> Op, params IOperationArgument<TBacking>[] Operands) where TBacking : INumber<TBacking>
+{
+    public override string ToString()
+    {
+        return $"{Op} {Operands.Select(x => x.Value.ToString()).Aggregate((x, y) => $"{x} {y}")}";
+    }
+}
--- SmallLang/LinearIR/OpcodeWrapper.cs
+++ SmallLang/LinearIR/OpcodeWrapper.cs
@@ -1,11 +1,16 @@
 using Common.LinearIR;
 
 namespace SmallLang.LinearIR;
+
 public record class OpcodeWrapper(Opcode Op) : IOperationArgument<uint>
 {
     public uint Value => (uint)Op;
     public static implicit operator OpcodeWrapper(Opcode inp)
     {
         return new(inp);
+    }
+    public override string ToString()
+    {
+        return Op.ToString();
     }
 }

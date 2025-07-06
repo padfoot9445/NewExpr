323f9b066df5f8de2315b562d2b95455cbf06fa0
padfoot9445 <yipy214@gmail.com>
2025-07-06 22:37:55 +0100

    VarMods

---
+++ SmallLang/Metadata/VariableModifiers.cs
@@ -0,0 +1,12 @@
+namespace SmallLang.Metadata;
+
+[Flags]
+public enum VariableModifiers : byte
+{
+    None = 0,
+    Ref = 1,
+    Readonly = 1 << 1,
+    Frozen = 1 << 2,
+    Immut = 1 << 3,
+    Copy = 1 << 4,
+}

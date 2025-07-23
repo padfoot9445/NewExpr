65e89353e2d1de9911874159c8800546180ccd1f
padfoot9445 <yipy214@gmail.com>
2025-07-23 18:02:39 +0100

    TypeErrorException added to have fields Expected, Actual, Position, and AdditionalMessage

--- SmallLang/TypeErrorException.cs
+++ SmallLang/TypeErrorException.cs
@@ -4,10 +4,15 @@ namespace SmallLang;
 
 public class TypeErrorException : ExpaException
 {
-    public TypeErrorException(string message) : base(message)
-    {
-    }
+    public readonly SmallLangType Expected;
+    public readonly SmallLangType Actual;
+    public readonly int Position;
+    public readonly string? AdditionalMessage;
     public TypeErrorException(SmallLangType Expected, SmallLangType Actual, int Position, string? Message = null) : base($"Expected A value of type {Expected} but was {Actual} at {Position}." + (Message is null ? "" : $"Additional Context: {Message}"))
     {
+        this.Expected = Expected;
+        this.Actual = Actual;
+        this.Position = Position;
+        this.AdditionalMessage = Message;
     }
 }

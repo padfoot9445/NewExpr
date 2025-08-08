5c9e24c0667d38cd6c8e8cda5e6d2ae321800734
padfoot9445 <yipy214@gmail.com>
2025-08-08 23:47:36 +0100

    Added opertors and Add and Next

--- Common/Metadata/Pointer.cs
+++ Common/Metadata/Pointer.cs
@@ -6,4 +6,15 @@ namespace Common.Metadata;
 public record class Pointer<TBacking>(int BackingValue) : NumberWrapper<int, TBacking>(BackingValue)
 where TBacking : IBinaryInteger<TBacking>, IMinMaxValue<TBacking>
 {
+    public Pointer<TBacking> Next() => Add(1);
+    public Pointer<TBacking> Add(int amount) => this with { BackingValue = BackingValue + amount };
+    public static Pointer<TBacking> GetDefault() => new(0);
+    public static bool operator <(Pointer<TBacking> self, Pointer<TBacking> other)
+    {
+        return self.BackingValue < other.BackingValue;
+    }
+    public static bool operator >(Pointer<TBacking> self, Pointer<TBacking> other)
+    {
+        return self.BackingValue > other.BackingValue;
+    }
 }

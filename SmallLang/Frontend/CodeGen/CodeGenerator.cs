4040158cab18f32baa4860440807b51adce526fc
padfoot9445 <yipy214@gmail.com>
2025-08-09 00:48:45 +0100

    CodeGenerator stub

---
+++ SmallLang/Frontend/CodeGen/CodeGenerator.cs
@@ -0,0 +1,11 @@
+namespace SmallLang.Frontend.CodeGen;
+
+public class CodeGenerator(Node RootNode)
+{
+    private readonly Data data = new();
+    public Data Parse()
+    {
+        throw new NotImplementedException();
+        return data;
+    }
+}

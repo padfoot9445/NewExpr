95f7cc97c0f02c145e974fb83e2b76ed91696682
padfoot9445 <yipy214@gmail.com>
2025-08-09 18:02:14 +0100

    Return

--- SmallLang/LinearIR/Opcode.cs
+++ SmallLang/LinearIR/Opcode.cs
@@ -4,4 +4,5 @@ public enum Opcode : OpcodeBackingType
 {
     JMP,//JMP [Chunk ID] #Jumps unconditionally to the chunk given by the chunk-id
     BRZ, //BRZ [Chunk0 ID] [Chunk1 ID] #if the value at the top of the stack is 0, jump to Chunk1, otherwise jump to Chunk0
+    RET, //RET #Returns control to the calling chunk. Return values should be on the stack.
 }

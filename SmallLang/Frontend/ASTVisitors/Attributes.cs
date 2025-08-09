87489a0a3666be940e5d3f65ed5d3606b47b952c
padfoot9445 <yipy214@gmail.com>
2025-08-09 04:28:18 +0100

    Added LoopGUID

--- SmallLang/Frontend/ASTVisitors/Attributes.cs
+++ SmallLang/Frontend/ASTVisitors/Attributes.cs
@@ -4,7 +4,7 @@ using SmallLang.Metadata;
 
 namespace SmallLang.Frontend.ASTVisitors;
 
-public record class Attributes(List<SmallLangType>? DeclArgumentTypes = null, FunctionID<BackingNumberType>? FunctionID = null, SmallLangType? TypeOfExpression = null, SmallLangType? TypeLiteralType = null, VariableName? VariableName = null, VariableModifiers? VarMods = null, uint? SizeOfVariable = null, Scope? VariablesInScope = null) : IMetadata
+public record class Attributes(List<SmallLangType>? DeclArgumentTypes = null, FunctionID<BackingNumberType>? FunctionID = null, SmallLangType? TypeOfExpression = null, SmallLangType? TypeLiteralType = null, VariableName? VariableName = null, VariableModifiers? VarMods = null, uint? SizeOfVariable = null, Scope? VariablesInScope = null, LoopGUID? LoopGUID = null) : IMetadata
 {
     public Attributes() : this(DeclArgumentTypes: null) { }
     public void ForcedMerge(IMetadata other, bool PrioritizeOther = false)

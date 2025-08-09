4dd0c4b2fe93f5aae79a88381fe85b8f38681c41
padfoot9445 <yipy214@gmail.com>
2025-08-09 19:03:14 +0100

    IFNE

--- SmallLang/LinearIR/Opcode.cs
+++ SmallLang/LinearIR/Opcode.cs
@@ -11,5 +11,12 @@ public enum Opcode : OpcodeBackingType
             //     if Top of Stack == 2nd value on stack: {bottom}[3rd value, 2nd value, 1st value (aka top of stack)]{top}
             //         run CHUNKS[StartIndex + i * 2]
             //         run CHUNKS[StartIndex + Length * 2 + 2]
-
+    IFNE, //IFNE [StartingIndex] [Length]
+          //IFNN StartIndex Length
+          // for i in [1...LENGTH]:
+          //     run CHUNKS[i * 2 - 1]
+          //     if (POP) is TRUE:
+          //         run CHUNKS[i * 2]
+          //         jump to CHUNKS[LENGTH * 2 + 1]
+          // jump to CHUNKS[LENGTH * 2 + 1]
 }

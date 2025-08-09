5ba69c0789e9b263b1e78b2d10dabc3b701444c8
padfoot9445 <yipy214@gmail.com>
2025-08-09 04:11:10 +0100

    switched BRZ argument order around so it's (true, false)

--- SmallLang/LinearIR/Opcode.cs
+++ SmallLang/LinearIR/Opcode.cs
@@ -3,5 +3,5 @@ namespace SmallLang.LinearIR;
 public enum Opcode : OpcodeBackingType
 {
     JMP,//JMP [Chunk ID] #Jumps unconditionally to the chunk given by the chunk-id
-    BRZ, //BRZ [Chunk0 ID] [Chunk1 ID] #if the value at the top of the stack is 0, jump to Chunk0, otherwise jump to Chunk1
+    BRZ, //BRZ [Chunk0 ID] [Chunk1 ID] #if the value at the top of the stack is 0, jump to Chunk1, otherwise jump to Chunk0
 }

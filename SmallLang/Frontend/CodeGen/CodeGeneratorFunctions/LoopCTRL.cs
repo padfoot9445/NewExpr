b9344363b4365039fde7ba8a7166bd22217fbea8
padfoot9445 <yipy214@gmail.com>
2025-08-09 18:17:33 +0100

    LoopCTRL

---
+++ SmallLang/Frontend/CodeGen/CodeGeneratorFunctions/LoopCTRL.cs
@@ -0,0 +1,40 @@
+using System.Diagnostics;
+using Common.Tokens;
+using SmallLang.LinearIR;
+using SmallLang.Metadata;
+namespace SmallLang.Frontend.CodeGen;
+
+using static Opcode;
+public partial class CodeGenerator
+{
+    private void ParseLoopCTRL(Node Self)
+    {
+        //[Data: Break | Continue; Label?]
+
+        //ENTERINGCHUNK
+        bool IsBreak = Self.Data!.TT == TokenType.Break;
+        if (Self.Children.Count == 0)
+        {
+            if (IsBreak)
+            {
+                Emit(JMP, RCHUNK(2));
+            }
+            else
+            {
+                Emit(JMP, RCHUNK(-1));
+            }
+        }
+        else
+        {
+            (var Break, var Cont) = data.LoopData[(LoopGUID)Self.Attributes.GUIDOfLoopLabel!];
+            if (IsBreak)
+            {
+                Emit(JMP, Break);
+            }
+            else
+            {
+                Emit(JMP, Cont);
+            }
+        }
+    }
+}

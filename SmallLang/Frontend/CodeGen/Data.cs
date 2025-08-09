2f748de71c4bd2f4c9d837a65e4ec2a3ec19cf31
padfoot9445 <yipy214@gmail.com>
2025-08-09 04:27:18 +0100

    Added LoopData

--- SmallLang/Frontend/CodeGen/Data.cs
+++ SmallLang/Frontend/CodeGen/Data.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using Common.LinearIR;
 using Common.Metadata;
 using SmallLang.LinearIR;
+using SmallLang.Metadata;
 
 namespace SmallLang.Frontend.CodeGen;
 
@@ -10,4 +11,5 @@ public record class Data
     public Chunks Sections { get; init; } = new();
     public StaticallyAllocatedDataArea<VariableName, BackingNumberType> VariableSlots = new();
     public StaticallyAllocatedDataArea<VariableName, BackingNumberType> StaticDataArea = new();
+    public Dictionary<LoopGUID, (int ContinueDSTChunk, int BreakDSTChunk)> LoopData = new();
 }

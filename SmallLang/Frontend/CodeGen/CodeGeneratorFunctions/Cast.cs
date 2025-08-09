4d9c659fba66b54e6d677c2132636bf94b384044
padfoot9445 <yipy214@gmail.com>
2025-08-09 19:52:18 +0100

    Cast Stub

---
+++ SmallLang/Frontend/CodeGen/CodeGeneratorFunctions/Cast.cs
@@ -0,0 +1,13 @@
+using System.Diagnostics;
+using SmallLang.LinearIR;
+using SmallLang.Metadata;
+namespace SmallLang.Frontend.CodeGen;
+
+using static Opcode;
+public partial class CodeGenerator
+{
+    void Cast(Node self, SmallLangType dstType)
+    {
+        throw new NotImplementedException();
+    }
+}

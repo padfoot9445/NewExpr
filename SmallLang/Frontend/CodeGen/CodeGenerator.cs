a4149c5b4ad08e472aca1b4bd11573bed246977b
padfoot9445 <yipy214@gmail.com>
2025-08-09 04:08:02 +0100

    Chunk function

--- SmallLang/Frontend/CodeGen/CodeGenerator.cs
+++ SmallLang/Frontend/CodeGen/CodeGenerator.cs
@@ -18,7 +18,8 @@ public partial class CodeGenerator(Node RootNode)
         Emit(new Operation<Opcode, byte>((OpcodeWrapper)op, args));
     }
     void NewChunk() => data.Sections.NewChunk();
-
+    int CHUNKI(int ChunkRelOffset) => CurrentChunkPtr + ChunkRelOffset; //first chunk is chunk1; entering chunk is chunk0
+    IOperationArgument<BackingNumberType> CHUNK(int ChunkRelOffset) => new GenericNumberWrapper<int>(CHUNKI(ChunkRelOffset));
     private readonly Data data = new();
     public Data Parse()
     {

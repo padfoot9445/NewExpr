1f15c947bf6327f9603a58cb44c2b2dceb5a8714
padfoot9445 <yipy214@gmail.com>
2025-08-09 18:35:25 +0100

    SWITCH

--- SmallLang/LinearIR/Opcode.cs
+++ SmallLang/LinearIR/Opcode.cs
@@ -5,4 +5,11 @@ public enum Opcode : OpcodeBackingType
     JMP,//JMP [Chunk ID] #Jumps unconditionally to the chunk given by the chunk-id
     BRZ, //BRZ [Chunk0 ID] [Chunk1 ID] #if the value at the top of the stack is 0, jump to Chunk1, otherwise jump to Chunk0
     RET, //RET #Returns control to the calling chunk. Return values should be on the stack.
+    SWITCH, //SWITCH [Length], [StartingIndex] # SWITCH Length StartIndex
+            // For i in [1...Length]:
+            //     run CHUNKS[StartIndex + i * 2 - 1]
+            //     if Top of Stack == 2nd value on stack: {bottom}[3rd value, 2nd value, 1st value (aka top of stack)]{top}
+            //         run CHUNKS[StartIndex + i * 2]
+            //         run CHUNKS[StartIndex + Length * 2 + 2]
+
 }

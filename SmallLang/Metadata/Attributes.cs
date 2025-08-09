0938d10de07a53d1a44ff6485dff840cd273e43a
padfoot9445 <yipy214@gmail.com>
2025-08-09 18:16:27 +0100

    Added GUIDOfLoopLabel

--- SmallLang/Metadata/Attributes.cs
+++ SmallLang/Metadata/Attributes.cs
@@ -3,7 +3,7 @@ using Common.Metadata;
 
 namespace SmallLang.Metadata;
 
-public record class Attributes(List<SmallLangType>? DeclArgumentTypes = null, FunctionID<BackingNumberType>? FunctionID = null, SmallLangType? TypeOfExpression = null, SmallLangType? TypeLiteralType = null, VariableName? VariableName = null, VariableModifiers? VarMods = null, uint? SizeOfVariable = null, Scope? VariablesInScope = null, LoopGUID? LoopGUID = null) : IMetadata
+public record class Attributes(List<SmallLangType>? DeclArgumentTypes = null, FunctionID<BackingNumberType>? FunctionID = null, SmallLangType? TypeOfExpression = null, SmallLangType? TypeLiteralType = null, VariableName? VariableName = null, VariableModifiers? VarMods = null, uint? SizeOfVariable = null, Scope? VariablesInScope = null, LoopGUID? LoopGUID = null, LoopGUID? GUIDOfLoopLabel = null) : IMetadata
 {
     public Attributes() : this(DeclArgumentTypes: null) { }
     public void ForcedMerge(IMetadata other, bool PrioritizeOther = false)

85e658ef0081f6146d28af188b779f9d6836a76b
padfoot9445 <yipy214@gmail.com>
2025-08-09 01:54:49 +0100

    Added dispatch helper methods

--- Common/Backend/CodeGenerator.cs
+++ Common/Backend/CodeGenerator.cs
@@ -1,12 +1,39 @@
 using Common.AST;
 using Common.Evaluator;
-
 namespace Common.Backend;
+
 public class CodeGenerator
 {
-    public void Evaluate<T, A>(DynamicASTNode<T, A> node, IDynamicASTVisitor<T, A> visitor) where A : IMetadata, new()
+    public static TReturn Dispatch<TN, TAC, TReturn>
+    (
+        DynamicASTNode<TN, TAC> node,
+        params
+        (
+            TN,
+            Func<DynamicASTNode<TN, TAC>, TReturn>
+        )[] Cases
+    )
+    where TAC : IMetadata, new()
     {
-        throw new NotImplementedException();
-        node.Walk(visitor, out bool _);
+        foreach ((var type, var Function) in Cases)
+        {
+            if (node.NodeType!.Equals(type)) return Function(node);
+        }
+        throw new Exception($"Ran out of cases when switching on {node.NodeType}");
+    }
+    public static void Dispatch<TN, TA>
+    (
+        DynamicASTNode<TN, TA> node,
+        params
+        (
+            TN,
+            Action<DynamicASTNode<TN, TA>>
+        )[] Cases
+    )
+    where TA : IMetadata, new()
+    {
+        Dispatch(node, Cases.Select<(TN, Action<DynamicASTNode<TN, TA>>), (TN, Func<DynamicASTNode<TN, TA>, bool>)>(x => (x.Item1, y => { x.Item2(y); return true; })).ToArray());
+        //Don't question it too hard
+        //this takes the Action<Node> of the cases, turns them into Func<Node, bool>, and then calls Dispatch with return-type bool and then discards the return
     }
 }

b8f6ea939c025fd0810ff29ea9d4bc589221c952
padfoot9445 <yipy214@gmail.com>
2025-08-14 02:12:42 +0100

    GCT in attributes

--- SmallLang/IR/Metadata/Attributes.cs
+++ SmallLang/IR/Metadata/Attributes.cs
@@ -3,7 +3,7 @@ using Common.Metadata;
 
 namespace SmallLang.IR.Metadata;
 
-public record class Attributes(List<SmallLangType>? DeclArgumentTypes = null, FunctionID<BackingNumberType>? FunctionID = null, SmallLangType? TypeOfExpression = null, SmallLangType? TypeLiteralType = null, VariableName? VariableName = null, VariableModifiers? VarMods = null, uint? SizeOfVariable = null, Scope? VariablesInScope = null, LoopGUID? LoopGUID = null, LoopGUID? GUIDOfLoopLabel = null) : IMetadata
+public record class Attributes(List<SmallLangType>? DeclArgumentTypes = null, FunctionID<BackingNumberType>? FunctionID = null, SmallLangType? TypeOfExpression = null, SmallLangType? TypeLiteralType = null, VariableName? VariableName = null, VariableModifiers? VarMods = null, uint? SizeOfVariable = null, Scope? VariablesInScope = null, LoopGUID? LoopGUID = null, LoopGUID? GUIDOfLoopLabel = null, SmallLangType? GreatestCommonType = null) : IMetadata
 {
     public Attributes() : this(DeclArgumentTypes: null) { }
     public void ForcedMerge(IMetadata other, bool PrioritizeOther = false)

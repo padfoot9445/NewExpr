cb7cf5d7b7e636bf7087681fdbc4252b9afb54c9
padfoot9445 <yipy214@gmail.com>
2025-08-19 23:19:10 +0100

    changed 'primary' to primary_ctor

--- Common/CodeGenerationScripts/codegenframework.py
+++ Common/CodeGenerationScripts/codegenframework.py
@@ -17,10 +17,10 @@ def code_block(name: str, keyword: str | Keywords, content: list[str], prefix: l
     return f"{" ".join(modifiers)} {"".join(prefix)} {keyword if isinstance(keyword, str) else keyword.value} {name} {" ".join(affixes)}\n{{\n{TAB}{f"\n{TAB}".join(content)}\n}}"
 def write_block(block: str, out: Any):
     print(block, file=out)
-def code_class(name: str, content: list[str], modifiers: list[str] = [], primary: list[str] = [], parents: list[str] = [], constraints: list[str] = []) -> str:
+def code_class(name: str, content: list[str], modifiers: list[str] = [], primary_ctor: list[str] = [], parents: list[str] = [], constraints: list[str] = []) -> str:
     return code_block(name, keyword=Keywords.Class, content=content, modifiers=modifiers, affixes=
         [
-            f"({", ".join(primary)})",
+            f"({", ".join(primary_ctor)})",
             ":",
             ", ".join(parents),
             "\n".join(constraints)

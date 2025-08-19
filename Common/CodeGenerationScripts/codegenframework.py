9b45b0669371eb8db42da75ae553fb891e2126a3
padfoot9445 <yipy214@gmail.com>
2025-08-19 23:28:08 +0100

    added accessmodifiers type union to modifiers param

--- Common/CodeGenerationScripts/codegenframework.py
+++ Common/CodeGenerationScripts/codegenframework.py
@@ -13,11 +13,11 @@ class AccessModifiers(Enum):
     Empty = ""
 def get_value(possible_enum: str | Enum) -> str: return possible_enum if isinstance(possible_enum, str) else possible_enum.value
 def optional_str(condition: Any | None, string: str | None) -> str: return cast(str, string) if condition is not None else ""
-def code_block(name: str, keyword: str | Keywords, content: list[str], prefix: list[str] = [], affixes: list[str] = [], modifiers: list[str] = []) -> str:
-    return f"{" ".join(modifiers)} {"".join(prefix)} {keyword if isinstance(keyword, str) else keyword.value} {name} {" ".join(affixes)}\n{{\n{TAB}{f"\n{TAB}".join(content)}\n}}"
+def code_block(name: str, keyword: str | Keywords, content: list[str], prefix: list[str] = [], affixes: list[str] = [], modifiers: list[str | AccessModifiers] = []) -> str:
+    return f"{" ".join(get_value(i) for i in modifiers)} {"".join(prefix)} {keyword if isinstance(keyword, str) else keyword.value} {name} {" ".join(affixes)}\n{{\n{TAB}{f"\n{TAB}".join(content)}\n}}"
 def write_block(block: str, out: Any):
     print(block, file=out)
-def code_class(name: str, content: list[str], modifiers: list[str] = [], primary_ctor: list[str] = [], parents: list[str] = [], constraints: list[str] = []) -> str:
+def code_class(name: str, content: list[str], modifiers: list[str | AccessModifiers] = [], primary_ctor: list[str] = [], parents: list[str] = [], constraints: list[str] = []) -> str:
     return code_block(
         name,
         keyword=Keywords.Class,

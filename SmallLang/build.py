a8965a89a7c09c5d99bfabe52da18dc5c34241ca
padfoot9445 <yipy214@gmail.com>
2025-08-20 19:34:06 +0100

    added old-file-deleter

--- SmallLang/build.py
+++ SmallLang/build.py
@@ -7,10 +7,12 @@ import os
 import sys
 import yaml
 from time import time
-
+import glob
 
 Command = tuple[list[str | Path], str]
 working_directory = Path(os.path.dirname(__file__))
+
+TIME_ROUND: int = 2
 
 
 custom_code_generators: list[Callable[[], None]] = [
@@ -19,6 +21,9 @@ custom_code_generators: list[Callable[[], None]] = [
 ]
 
 if __name__ == "__main__":
+    #get generator-dst-directories to remove
+    dst_directories: list[Path] = []
+
     #get build steps:
     build_steps: list[Command] = []
 
@@ -38,6 +43,11 @@ if __name__ == "__main__":
                     [sys.executable, code_generation_scripts_directory/generator, working_directory/file_path, working_directory/dst],
                     display_name
                 ))
+
+                #append dst to dst_directories to facilitate removal
+                
+                if Path.is_dir(working_directory/dst):
+                    dst_directories.append(working_directory/dst)
 
 
     fmt_command: Command = (["dotnet", "format", "--no-restore"], "Dotnet-Format")
@@ -77,11 +87,19 @@ if __name__ == "__main__":
     
 
     total_time = time()
+
+    #delete old files
+    del_time = time()
+    for dst_dir in dst_directories:
+        for file in glob.glob(str(dst_dir/"*")):
+            os.remove(file)
+    print(f"BUILD: Clear Destination Directories succeeded in {round(time() - del_time, TIME_ROUND)}s")
+
     #run build steps
     for command, msg in build_steps:
         t = time()
         command = [str(i) for i in command]
         with open(log_file_path, "a") as file_path:
             subprocess.run(command, check=True, stdout=file_path)
-        print(f"BUILD: {msg} succeeded in {round(time() - t, 2)}s")
-    print(f"BUILD: Build Steps executed in {round(time() - total_time,2)}s")
+        print(f"BUILD: {msg} succeeded in {round(time() - t, TIME_ROUND)}s")
+    print(f"BUILD: Build Steps executed in {round(time() - total_time, TIME_ROUND)}s")

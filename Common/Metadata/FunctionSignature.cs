fe535895e709cbc4e382e65337ab28c52be5b600
padfoot9445 <yipy214@gmail.com>
2025-08-21 02:04:19 +0100

    bugfix/fixed accidental SmallLang namespace

--- Common/Metadata/FunctionSignature.cs
+++ Common/Metadata/FunctionSignature.cs
@@ -1,7 +1,7 @@
 using System.Numerics;
 using Common.Metadata;
 
-namespace SmallLang.Metadata;
+namespace Common.Metadata;
 
 public record class FunctionSignature<T, TType>(string Name, FunctionID<T> ID, IMetadataTypes<TType> RetVal, List<IMetadataTypes<TType>> ArgTypes)
 where T : IBinaryInteger<T>, IMinMaxValue<T>

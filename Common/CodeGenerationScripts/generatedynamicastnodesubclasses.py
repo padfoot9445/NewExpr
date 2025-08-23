c873b844dfdc307de681be66b14aec4052d9086f
padfoot9445 <yipy214@gmail.com>
2025-08-23 02:12:50 +0100

    feat/interface groups

--- Common/CodeGenerationScripts/generatedynamicastnodesubclasses.py
+++ Common/CodeGenerationScripts/generatedynamicastnodesubclasses.py
@@ -131,3 +131,18 @@ if __name__ == "__main__":
                 ),
                 file
             )
+    
+    #write interfaces for attribute groups
+    for attribute_group in config["attribute groups"]:
+        name = f"IHasAttributes_{"_".join(attribute for attribute in attribute_group)}"
+        with open(output_directory/f"{name}.cs", "w") as file:
+            write_header(raw_config, file)
+            write_block(
+                code_block(
+                    name=name,
+                    keyword="interface",
+                    content=[],
+                    affixes=[":", ", ".join(f"IHasAttribute{attribute}" for attribute in attribute_group)]
+                ),
+                file
+            )

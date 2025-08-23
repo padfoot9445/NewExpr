6faae2025f7e14c865448b4f0e7cbed93e8e523b
padfoot9445 <yipy214@gmail.com>
2025-08-23 05:09:59 +0100

    feat/exported join two parent group function

--- Common/CodeGenerationScripts/attribute_group_generator.py
+++ Common/CodeGenerationScripts/attribute_group_generator.py
@@ -1,16 +1,16 @@
 from initializer import *
 from codegenframework import *
 from attribute_interface_generator import get_interface_name
+from collections.abc import Callable, Iterable
 
+join_two_parent_groups: Callable[[Iterable[str], Iterable[str]], str] = lambda j, k: (lambda x, y: ", ".join(x) + (", " if len(x) > 0 and len(y) > 0 else "") + ", ".join(y))(list(j), list(k))
 
 if __name__ == "__main__":
     _, output_directory, _, raw_config, config = initialize()
     #write interfaces for attribute groups
     for attribute_group in config:
         name = f"IHasAttributes_{"_".join(attribute for attribute in attribute_group["attributes"] + attribute_group["marker interfaces"])}"
-        joined_markers = ", ".join(attribute_group["marker interfaces"])
-        joined_attributes = ", ".join(get_interface_name(i) for i in attribute_group["attributes"])
-        both_joined = joined_markers + (", " if len(joined_markers) > 0 and len(joined_attributes) > 0 else "") + joined_attributes
+        both_joined = join_two_parent_groups(attribute_group["marker interfaces"], (get_interface_name(i) for i in attribute_group["attributes"]))
         with open(output_directory/f"{name}.cs", "w") as file:
             write_header(raw_config, file)
             write_block(

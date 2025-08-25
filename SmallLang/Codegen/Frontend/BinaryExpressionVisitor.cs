a3b1450ba88db967cea061051c072182b8f7d307
padfoot9445 <yipy214@gmail.com>
2025-08-25 20:24:25 +0100

    feat/INCOMPLETE/Filled out dispatch on Visit

---
+++ SmallLang/Codegen/Frontend/BinaryExpressionVisitor.cs
@@ -0,0 +1,29 @@
+    internal static void Visit(BinaryExpressionNode Self, CodeGenerator Driver)
+    {
+        Driver.EnteringChunk(() =>
+        {
+            Self.Switch
+            (
+                x => x.Data.TT,
+                (x, y) => x == y,
+
+
+                (TokenType.Equals, AssignmentVisitor),
+                (TokenType.LogicalImplies, NonAssignmentOperatorVisitor),
+                (TokenType.LogicalOr, NonAssignmentOperatorVisitor),
+                (TokenType.LogicalXor, NonAssignmentOperatorVisitor),
+                (TokenType.LogicalAnd, NonAssignmentOperatorVisitor),
+                (TokenType.Addition, NonAssignmentOperatorVisitor),
+                (TokenType.Subtraction, NonAssignmentOperatorVisitor),
+                (TokenType.Multiplication, NonAssignmentOperatorVisitor),
+                (TokenType.Division, NonAssignmentOperatorVisitor),
+                (TokenType.Exponentiation, NonAssignmentOperatorVisitor),
+                (TokenType.BitwiseOr, NonAssignmentOperatorVisitor),
+                (TokenType.BitwiseXor, NonAssignmentOperatorVisitor),
+                (TokenType.BitwiseAnd, NonAssignmentOperatorVisitor),
+                (TokenType.BitwiseLeftShift, NonAssignmentOperatorVisitor),
+                (TokenType.BitwiseRightShift, NonAssignmentOperatorVisitor),
+                (TokenType.BitwiseNegation, NonAssignmentOperatorVisitor)
+            );
+        });
+    }

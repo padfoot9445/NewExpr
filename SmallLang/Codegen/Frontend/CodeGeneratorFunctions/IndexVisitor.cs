3770c9e94eacba302bb0c97ec5adf4dc912a6d19
padfoot9445 <yipy214@gmail.com>
2025-08-26 17:37:03 +0100

    UNFINISHED/IndexVisitor

---
+++ SmallLang/Codegen/Frontend/CodeGeneratorFunctions/IndexVisitor.cs
@@ -0,0 +1,34 @@
+using System.Diagnostics;
+using Common.Tokens;
+using SmallLang.IR.AST;
+using SmallLang.IR.AST.Generated;
+using SmallLang.IR.LinearIR;
+using SmallLang.IR.Metadata;
+
+namespace SmallLang.CodeGen.Frontend.CodeGeneratorFunctions;
+
+internal static class IndexVisitor
+{
+    internal static void Visit(IndexNode Self, CodeGenerator Driver)
+    {
+        Debug.Assert(Self.Expression1.TypeOfExpression is not null);
+        Driver.EnteringChunk(() =>
+        {
+            var StructRegister = Driver.GetRegisters(Self.Expression1.TypeOfExpression).First();
+            Driver.Exec(Self.Expression1);
+            Driver.Emit(HighLevelOperation.PushFromRegister(StructRegister, Self.Expression1.TypeOfExpression.Size));
+
+
+            if (Self.TypeOfExpression == TypeData.Dict)
+            {
+
+            }
+            else
+            {
+
+            }
+
+            Driver.Next();
+        });
+    }
+}

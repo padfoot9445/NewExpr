1253932b52682692e80ee8755b2098ac7319a0b7
padfoot9445 <yipy214@gmail.com>
2025-08-26 22:49:14 +0100

    feat/Extracted CallFunction method from FunctionCallVisitor

--- SmallLang/Codegen/Frontend/CodeGeneratorFunctions/FunctionCallVisitor.cs
+++ SmallLang/Codegen/Frontend/CodeGeneratorFunctions/FunctionCallVisitor.cs
@@ -16,19 +16,26 @@ internal static class FunctionCallVisitor
 
         Driver.EnteringChunk(() =>
         {
-            foreach (var i in Self.ArgList)
-            {
-                Driver.Cast(i.Expression, i.Expression.ExpectedTypeOfExpression!); //no need to load into register because we want it on the stack
-            }
-
-
-            var ReturnRegister = Driver.GetRegisters(Self).First();
-
-            FunctionID<BackingNumberType> ID = Self.FunctionID!;
-            Driver.Emit(HighLevelOperation.Call(ID));
-
-            Driver.Emit(HighLevelOperation.LoadFromStack(ReturnRegister, Self.TypeOfExpression.Size));
-            Driver.Emit(HighLevelOperation.PushFromRegister(ReturnRegister, Self.TypeOfExpression.Size)); //pop and push because we want to have a place to store the retval for optimising purposes
+            CallFunction(Self, Self.ArgList, Driver);
+            Driver.Next();
         });
+    }
+
+    internal static void CallFunction<T1>(T1 Self, IEnumerable<ArgListElementNode> ArgList, CodeGenerator Driver)
+    where T1 : ISmallLangNode, IHasAttributeFunctionID, IHasAttributeTypeOfExpression
+    {
+        foreach (var i in ArgList)
+        {
+            Driver.Cast(i.Expression, i.Expression.ExpectedTypeOfExpression!); //no need to load into register because we want it on the stack
+        }
+
+
+        var ReturnRegister = Driver.GetRegisters(Self).First();
+
+        FunctionID<BackingNumberType> ID = Self.FunctionID!;
+        Driver.Emit(HighLevelOperation.Call(ID));
+
+        Driver.Emit(HighLevelOperation.LoadFromStack(ReturnRegister, Self.TypeOfExpression!.Size));
+        Driver.Emit(HighLevelOperation.PushFromRegister(ReturnRegister, Self.TypeOfExpression.Size)); //pop and push because we want to have a place to store the retval for optimising purposes
     }
 }

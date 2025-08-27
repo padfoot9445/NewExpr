dada4b5f31f0c265471d94d25e1f45ccbecd9084
padfoot9445 <yipy214@gmail.com>
2025-08-27 15:05:17 +0100

    patch/fixed some bugs in the output and also in the code
    - used to call str.splitext which is not a thing
    - added Path(program_file).resolve() instead of program_file for more clarity (names are not accurate here)
    - added missing semicolon in output code

--- Common/CodeGenerationScripts/ExamplePrograms.py
+++ Common/CodeGenerationScripts/ExamplePrograms.py
@@ -8,13 +8,12 @@ if __name__ == "__main__":
 
     with open(output_dir/"ExamplePrograms.cs", "w") as file:
         write_header(raw_config, file)
-        class_content: list[str] = [] 
-
-        for root, dirs, files in os.walk(config["programs path"]):
+        class_content: list[str] = []
+        for root, dirs, files in os.walk(Path(config["programs path"]).resolve()):
             for program_file in files:
                 if os.path.splitext(program_file)[1] in config["extensions"]:
                     with open(Path(root)/program_file) as opened_program_file:
-                        class_content.append(f"public static readonly string {program_file.splitext[0]} = @\"\n{"\n".join(i for i in opened_program_file.readlines())}\n\"")
+                        class_content.append(f"public static readonly string {os.path.splitext(program_file)[0]} = @\"\n{"\n".join(i for i in opened_program_file.readlines())}\n\";")
         
         write_block(code_class(
             name="ExamplePrograms",

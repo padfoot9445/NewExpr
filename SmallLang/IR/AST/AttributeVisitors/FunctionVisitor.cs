a09804a19a9cf554e266634a47eaa6e10ead6a5c
padfoot9445 <yipy214@gmail.com>
2025-08-30 00:11:40 +0100

    feat!/made Define take a type parameter

--- SmallLang/IR/AST/AttributeVisitors/FunctionVisitor.cs
+++ SmallLang/IR/AST/AttributeVisitors/FunctionVisitor.cs
@@ -8,7 +8,7 @@ public static partial class AttributeVisitor
     static void Evaluate(FunctionNode node)
     {
         DoIfNotNull(() => node.VariableName = node.Scope!.GetName(node.Data.Lexeme), node.Scope);
-        DoIfNotNull(() => node.Scope!.Define(node.VariableName!), node.VariableName, node.Scope);
+        DoIfNotNull(() => node.Scope!.Define(node.VariableName!, TypeData.Void), node.VariableName, node.Scope);
         node.FunctionBody.Scope = new Scope { Parent = node.Scope };
     }
 }

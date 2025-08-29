ff53229e22dc008c114491b31754d8f0d9878c50
padfoot9445 <yipy214@gmail.com>
2025-08-29 18:57:31 +0100

    feat/made AttributeVisitor class partial

--- SmallLang/IR/AST/ASTVisitors/AttributeVisitor.cs
+++ SmallLang/IR/AST/ASTVisitors/AttributeVisitor.cs
@@ -1,6 +1,6 @@
 namespace SmallLang.IR.AST.ASTVisitors;
 
-public static class AttributeVisitor
+public static partial class AttributeVisitor
 {
     public static void BuildAttributes(this SmallLangNode node)
     {

474cec1a74d363aef18fee2d0004995f91b8eb0f
padfoot9445 <yipy214@gmail.com>
2025-08-29 23:56:29 +0100

    feat/changed NamesDefinedInThisScope to be private, not public

--- SmallLang/IR/Metadata/Scope.cs
+++ SmallLang/IR/Metadata/Scope.cs
@@ -4,7 +4,7 @@ public record Scope
 {
     //Should be set by the parent in the case of implicit sections, or set by the child (self) in case of explicit sections
     public required Scope? Parent { get; init; }
-    public HashSet<VariableName> NamesDefinedInThisScope { get; } = new();
+    private HashSet<VariableName> NamesDefinedInThisScope { get; } = new();
     public VariableName GetName(string name) => throw new NotImplementedException();
     public bool IsDefined(string name) => IsDefined(GetName(name));
     public bool IsDefined(VariableName name) => NamesDefinedInThisScope.Contains(name) || (Parent is not null && Parent.IsDefined(name));

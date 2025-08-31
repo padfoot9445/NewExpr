315b47a04f1d662815cc746d87aa3fdfa8c8a01e
padfoot9445 <yipy214@gmail.com>
2025-08-31 20:44:22 +0100

    feat/made SmallLangNode : IHasAttributeScopeSettable

--- SmallLang/IR/AST/SmallLangNode.cs
+++ SmallLang/IR/AST/SmallLangNode.cs
@@ -1,14 +1,15 @@
 using System.Collections;
+using SmallLang.IR.AST.Generated;
 using SmallLang.IR.Metadata;
 
 namespace SmallLang.IR.AST;
 
-public abstract record class SmallLangNode : ISmallLangNode
+public abstract record class SmallLangNode : ISmallLangNode, IHasAttributeScope, IHasAttributeScopeSettable
 {
     protected abstract IEnumerable<ISmallLangNode?> Children { get; set; }
     public IEnumerable<ISmallLangNode> ChildNodes => Children.Where(x => x != null).Select(x => x!);
 
     public abstract T AcceptVisitor<T>(ISmallLangNode? Parent, ISmallLangNodeVisitor<T> visitor);
 
-    public Scope? Scope { get; internal set; } = null;
+    public Scope? Scope { get; set; } = null;
 }

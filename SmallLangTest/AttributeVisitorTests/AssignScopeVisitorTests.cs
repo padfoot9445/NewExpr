b74ec0a490ff4a418f82958fc1d58cdabc658f9f
padfoot9445 <yipy214@gmail.com>
2025-09-01 01:48:26 +0100

    feat/Unit tests for AssignScopeVisitor

---
+++ SmallLangTest/AttributeVisitorTests/AssignScopeVisitorTests.cs
@@ -0,0 +1,34 @@
+using Common.AST;
+using SmallLang.IR.AST;
+using SmallLang.IR.AST.ASTVisitors.AttributeEvaluators;
+using SmallLangTest;
+using SmallLangTest.Generated;
+namespace SmallLangTest.AttributeVisitorTests;
+
+[TestFixture]
+public class AssignScopeVisitorTest
+{
+
+    [Test, Timeout(5000)]
+    public void All_Programs__AssignScope_BeginVisiting__Does_Not_Throw()
+    {
+        foreach (var program in ExamplePrograms.AllPrograms)
+        {
+            var AssignScopeVisitor = new AssignScopeVisitor();
+            Assert.That(() => AssignScopeVisitor.BeginVisiting(ParserTest.Parse(program)), Throws.Nothing, message: program);
+        }
+    }
+    [Test]
+    public void All_Programs__AssignScope_BeginVisiting__No_Scope_Is_Null()
+    {
+        foreach (var program in ExamplePrograms.AllPrograms)
+        {
+            var AssignScopeVisitor = new AssignScopeVisitor();
+            var ast = ParserTest.Parse(program);
+            AssignScopeVisitor.BeginVisiting(ast);
+
+            Assert.That(ast.Flatten().Select(x => x.Scope is null).Any(), Is.False);
+        }
+    }
+
+}

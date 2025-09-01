ac3a18f5a96846111eefc1c01645200c9b2ca2f6
padfoot9445 <yipy214@gmail.com>
2025-09-02 00:26:01 +0100

    feat/List ctor

--- Common/Metadata/FunctionSignature.cs
+++ Common/Metadata/FunctionSignature.cs
@@ -8,6 +8,7 @@ public record class FunctionSignature<T, TType>(string Name, FunctionID<T> ID, T
 where T : IBinaryInteger<T>, IMinMaxValue<T>
 where TType : IMetadataTypes<TType>
 {
+    public FunctionSignature(string Name, FunctionID<T> ID, TType RetVal, List<TType> ArgTypes) : this(Name, ID, RetVal, ArgTypes.AsReadOnly()) { }
     public virtual bool Equals(FunctionSignature<T, TType>? Other)
     {
         return Other is not null && Name == Other.Name && ID == Other.ID && RetVal.Equals(Other.RetVal) && ArgTypes.Count == Other.ArgTypes.Count && ArgTypes.Zip(Other.ArgTypes).Select(x => x.First.Equals(x.Second)).All(x => x);

4553c6793ce4284ab1a0c27716196f50a9dfc3c4
padfoot9445 <yipy214@gmail.com>
2025-09-02 22:36:20 +0100

    patch/added TypeLiteralTypeVisitor pass before doing FunctionID

--- SmallLangTest/FunctionIDTest.cs
+++ SmallLangTest/FunctionIDTest.cs
@@ -19,6 +19,10 @@ public class FunctionIDTests
 
             AssignScopeVisitor.BeginVisiting(AST);
 
+            var TLTVisitor = new TypeLiteralTypeVisitor();
+
+            TLTVisitor.BeginVisiting(AST);
+
             yield return (AST, program);
         }
     }

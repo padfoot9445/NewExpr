239f39a435aebc40240150fbb77a014b2722ccf7
padfoot9445 <yipy214@gmail.com>
2025-09-02 22:28:16 +0100

    feat/FunctionID: Added GetNext factory method

--- Common/Metadata/FunctionID.cs
+++ Common/Metadata/FunctionID.cs
@@ -6,5 +6,6 @@ namespace Common.Metadata;
 public record class FunctionID<TBackingType>(uint BackingValue) : NumberWrapper<uint, TBackingType>(BackingValue)
 where TBackingType : IBinaryInteger<TBackingType>, IMinMaxValue<TBackingType>
 {
-
+    static uint Last { get; set; } = 0;
+    public static FunctionID<TBackingType> GetNext() => new(Last++);
 }

ea636e27dc1ab9c1239e54bdb663fbe527d84235
padfoot9445 <yipy214@gmail.com>
2025-09-05 00:01:28 +0100

    feat/GenericSLTTests

---
+++ SmallLangTest/AttributeVisitorTests/GenericSmallLangTypeExpressionTests.cs
@@ -0,0 +1,46 @@
+using Common.AST;
+using sly.parser;
+using SmallLang.IR.AST;
+using SmallLang.IR.AST.ASTVisitors;
+using SmallLang.IR.AST.ASTVisitors.AttributeEvaluators;
+using SmallLang.IR.AST.Generated;
+using SmallLangTest;
+using SmallLangTest.Generated;
+namespace SmallLangTest.AttributeVisitorTests;
+
+[TestFixture, CancelAfter(5000)]
+public class GenericSmallLangTypeExpressionTests
+{
+
+    internal static IEnumerable<(ISmallLangNode, string)> GetTestCases()
+    {
+        List<BaseASTVisitor> Evaluators = [new AssignScopeVisitor(), new VariableNameVisitor(), new TypeLiteralTypeVisitor()];
+
+        foreach (var program in ExamplePrograms.AllPrograms)
+        {
+            var AST = ParserTest.Parse(program);
+            foreach (var Evaluator in Evaluators)
+            {
+                Evaluator.BeginVisiting(AST);
+            }
+            yield return (AST, program);
+        }
+    }
+
+    [TestCaseSource(nameof(GetTestCases)), CancelAfter(5000)]
+    public void All_Programs__Generic_SLT_Visitor__BeginVisiting__Does_Not_Throw((ISmallLangNode ast, string program) input)
+    {
+
+        Assert.That(() => new GenericSLTypeVisitor().BeginVisiting(input.ast), Throws.Nothing, message: input.program);
+
+    }
+    [TestCaseSource(nameof(GetTestCases)), CancelAfter(5000)]
+    public void All_Programs__Generic_SLT_Visitor__BeginVisiting__No_GenericSLT_Is_Null((ISmallLangNode ast, string program) input)
+    {
+        new GenericSLTypeVisitor().BeginVisiting(input.ast);
+
+        Assert.That(input.ast.Flatten().OfType<IHasAttributeGenericSLType>().All(x => x.GenericSLType is not null), Is.True, message: $"{input.program}\n\n{string.Join('\n', input.ast.Flatten().OfType<IHasAttributeFunctionID>().Where(x => x.FunctionID is null).Select(x => x.ToString()))}");
+
+    }
+
+}

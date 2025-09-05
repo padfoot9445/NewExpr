1e6022084250e1197a14621217d93a0fceb7cc00
padfoot9445 <yipy214@gmail.com>
2025-09-05 20:20:57 +0100

    feat/ExpectedTypeOfExpressionVisitor

--- SmallLang/IR/AST/ASTVisitors/AttributeEvaluators/ExpectedTypeOfExpressionVisitor.cs
+++ SmallLang/IR/AST/ASTVisitors/AttributeEvaluators/ExpectedTypeOfExpressionVisitor.cs
@@ -1,3 +1,7 @@
+using System.Collections.Immutable;
+using System.Runtime.InteropServices.JavaScript;
+using Common.AST;
+using SmallLang.Exceptions;
 using SmallLang.IR.AST.Generated;
 
 namespace SmallLang.IR.AST.ASTVisitors.AttributeEvaluators;
@@ -10,10 +14,43 @@ internal class ExpectedTypeOfExpressionVisitor : BaseASTVisitor
         base.PreVisit(node);
     }
 
-    protected override ISmallLangNode VisitIndex(ISmallLangNode? Parent, IndexNode self)
+    protected override ISmallLangNode VisitFunctionCall(ISmallLangNode? Parent, FunctionCallNode self)
     {
-        NotNull(self.Expression1.GenericSLType);
-        self.ExpectedTypeOfExpression = self.Expression1.GenericSLType.ChildNodes.Last();
-        return base.VisitIndex(Parent, self);
+        var FunctionDefinitionNode = CurrentRootNode.Flatten()
+            .OfType<FunctionNode>()
+            .Single(x => x.FunctionName.VariableName == self.Identifier.VariableName);
+
+        var IndexableArgList =
+            FunctionDefinitionNode.TypeAndIdentifierCSV.Select(x => x.Type.GetGenericSLTFromLiteralType()).ToImmutableArray();
+
+        var FunctionDefinitionArgList = FunctionDefinitionNode.TypeAndIdentifierCSV
+            .Select(x => (x.Identifier.Data.Lexeme, x.Type.GetGenericSLTFromLiteralType()))
+            .ToDictionary();
+
+        bool SeenIdentifier = false;
+        foreach (var (i, v) in self.ArgList.Index())
+        {
+            if (v.Identifier is not null)
+            {
+                SeenIdentifier = true;
+
+
+                FunctionDefinitionArgList.TryGetValue(v.Identifier.Data.Lexeme, out var expectedTypeOfExpression);
+                v.ExpectedTypeOfExpression =
+                    expectedTypeOfExpression ??
+                    throw new ExpaException($"Argument label {v.Identifier.Data} was not recognized.");
+            }
+            else
+            {
+                if (SeenIdentifier)
+                    throw new ExpaException(
+                        $"ERROR at {v.Expression}: Positional parameters must be before any named parameters.");
+                else
+                {
+                    v.ExpectedTypeOfExpression = IndexableArgList[i];
+                }
+            }
+        }
+        return base.VisitFunctionCall(Parent, self);
     }
 }

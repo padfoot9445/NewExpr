692e85e0cc7811a2abccd554f3adbcb2cbb00ee9
padfoot9445 <yipy214@gmail.com>
2025-09-06 21:35:54 +0100

    feat/evaluated ast in compile using AttributeEvaluator

--- SmallLang/Drivers/HighToLowLevelCompilerDriver.cs
+++ SmallLang/Drivers/HighToLowLevelCompilerDriver.cs
@@ -1,5 +1,6 @@
 using SmallLang.CodeGen.Frontend;
 using SmallLang.IR.AST;
+using SmallLang.IR.AST.ASTVisitors.AttributeEvaluators;
 using SmallLang.IR.AST.Generated;
 
 namespace SmallLang.Drivers;
@@ -11,6 +12,7 @@ public class HighToLowLevelCompilerDriver
     {
         GetCodeGenerator ??= x => new CodeGenerator(x);
         var Ast = new Parser.Parser(Code).Parse<SectionNode>();
+        new AttributeEvaluator().BeginVisiting(Ast);
         var CodeGenerator = GetCodeGenerator(Ast);
         return CodeGenerator.Parse();
     }

c488fc14ba4534a03d12f61a628307beba7b5256
padfoot9445 <yipy214@gmail.com>
2025-09-06 01:05:55 +0100

    chore/merged nested if statement

--- SmallLang/Parser/SmallLangParser.cs
+++ SmallLang/Parser/SmallLangParser.cs
@@ -164,13 +164,12 @@ public partial class SmallLangParser
         var ThisCombined = new ExprSectionCombinedNode(TryCast<IExpressionNode>(Cond),
             ToSection<IStatementNode>(StatementExpr, nameof(NTIf)));
         List<ExprSectionCombinedNode> rc = [ThisCombined];
-        if (ElseNode is not null)
-            if (ElseNode.Section.Statements.Count > 0 && ElseNode.Section.Statements.First() is IfNode If)
-            {
-                Debug.Assert(ElseNode.Section.Statements.Count == 1);
-                rc.AddRange(If.ExprStatementCombineds);
-                ElseNode = If.Else;
-            }
+        if (ElseNode is not null && ElseNode.Section.Statements.Count > 0 && ElseNode.Section.Statements.First() is IfNode If)
+        {
+            Debug.Assert(ElseNode.Section.Statements.Count == 1);
+            rc.AddRange(If.ExprStatementCombineds);
+            ElseNode = If.Else;
+        }
 
         return new IfNode(rc, ElseNode);
 

5cc5fec1cb7a0d6f6c34c4cd06b11fa1ee2892b4
padfoot9445 <yipy214@gmail.com>
2025-09-06 01:11:58 +0100

    chore/removed DynamicASTNode which is no-longer used

--- Common/Evaluator/IDynamicASTVisitor.cs
+++
@@ -1,8 +0,0 @@
-using Common.AST;
-
-namespace Common.Evaluator;
-
-public interface IDynamicASTVisitor<T1, T2> where T2 : IMetadata, new()
-{
-    public Func<DynamicASTNode<T1, T2>?, DynamicASTNode<T1, T2>, bool> Dispatch(DynamicASTNode<T1, T2> node);
-}

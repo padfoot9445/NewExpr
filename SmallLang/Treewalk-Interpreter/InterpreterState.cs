ae668cce1908c25726c195123a707c20fe4e6809
padfoot9445 <yipy214@gmail.com>
2026-01-22 06:38:58 +0000

    feat/added empty ctor for InterpreterState

--- SmallLang/Treewalk-Interpreter/InterpreterState.cs
+++ SmallLang/Treewalk-Interpreter/InterpreterState.cs
@@ -2,4 +2,7 @@ using SmallLang.TreeWalkInterpreter.RuntimeObjects;
 
 namespace SmallLang.TreeWalkInterpreter;
 
-public record InterpreterState(Stack<IRunTimeObject> Stack);
+public record InterpreterState(Stack<IRunTimeObject> Stack)
+{
+  public InterpreterState() : this([]) { }
+}

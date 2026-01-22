89bc1f254800f3f1d850d6869ae9c9660681c233
padfoot9445 <yipy214@gmail.com>
2026-01-22 20:47:40 +0000

    patch/Visit[Primary] tests passed

--- SmallLang/TreewalkInterpreterTests/TreewalkInterpreterTests.cs
+++ SmallLang/TreewalkInterpreterTests/TreewalkInterpreterTests.cs
@@ -10,12 +10,10 @@ public class TreeWalkInterpreterTests
   [Test]
   public void VisitPrimary__Manually_Instantiated_Primary_Node__Modifies_State_Correctly()
   {
-    Assert.Multiple((() =>
-    {
-      var visitor = new TreeWalkInterpreter.TreeWalkInterpreter();
-      IEnumerable<PrimaryNode> PrimaryNodeTestCases =
-      [
-        new PrimaryNode(IToken.NewToken(TokenType.Number,
+    var visitor = new TreeWalkInterpreter.TreeWalkInterpreter();
+    IEnumerable<PrimaryNode> PrimaryNodeTestCases =
+    [
+      new PrimaryNode(IToken.NewToken(TokenType.Number,
           "1.234",
           -1)),
         new PrimaryNode(IToken.NewToken(TokenType.String,
@@ -24,15 +22,21 @@ public class TreeWalkInterpreterTests
           "abc")),
         new PrimaryNode(IToken.NewToken(TokenType.TrueLiteral, "", -1)),
         new PrimaryNode(IToken.NewToken(TokenType.FalseLiteral, "", -1))
-      ];
+    ];
 
-      foreach (var i in PrimaryNodeTestCases)
-      {
-        visitor.Visit(null, i);
-      }
+    foreach (var i in PrimaryNodeTestCases)
+    {
+      visitor.Visit(null, i);
+    }
 
-      object[] Expected = [false, true, "abc", 1.234];
-      Assert.That(visitor.State.Stack.Select(x => x.As<object>()), Is.EquivalentTo(Expected));
+    object[] Expected = [false, true, "abc", 1.234];
+    IRunTimeObject[] Results = visitor.State.Stack.ToArray();
+    Assert.Multiple((() =>
+    {
+      Assert.That(Results[0].AsVal<bool>(), Is.EqualTo(false));
+      Assert.That(Results[1].AsVal<bool>(), Is.EqualTo(true));
+      Assert.That(Results[2].AsRef<string>(), Is.EqualTo("abc"));
+      Assert.That(Results[3].AsVal<double>(), Is.EqualTo(1.234));
     }));
   }
 }

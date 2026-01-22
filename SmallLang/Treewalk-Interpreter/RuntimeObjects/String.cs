88a43ba9fa490b0b9213c4e66ef17cb6ca0bfb39
padfoot9445 <yipy214@gmail.com>
2026-01-22 20:03:22 +0000

    patch/used literal in FromTOken in String.cs instead of lexeme

--- SmallLang/Treewalk-Interpreter/RuntimeObjects/String.cs
+++ SmallLang/Treewalk-Interpreter/RuntimeObjects/String.cs
@@ -6,6 +6,6 @@ public record String(string Value) : IRunTimeBaseType<String, string>
 {
   public static String FromToken(IToken token)
   {
-    return new(token.Lexeme);
+    return new(token.Literal);
   }
 }

5601a3ce6a3a84249f1f99236cb3e9754ea04aa0
padfoot9445 <yipy214@gmail.com>
2026-01-22 06:35:14 +0000

    feat/added non-generic IRuntimeObject

--- SmallLang/Treewalk-Interpreter/RuntimeObjects/IRuntimeObject.cs
+++ SmallLang/Treewalk-Interpreter/RuntimeObjects/IRuntimeObject.cs
@@ -1,6 +1,10 @@
 namespace SmallLang.TreeWalkInterpreter.RuntimeObjects;
 
-public interface IRunTimeObject<out T>
+public interface IRunTimeObject<out T> : IRunTimeObject
 {
   public T Value { get; }
+}
+
+public interface IRunTimeObject
+{
 }

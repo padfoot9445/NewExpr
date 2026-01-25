551936c179a4b2983402333b0d895b0c56924e0a
padfoot9445 <yipy214@gmail.com>
2026-01-25 15:26:40 +0000

    feat/Visit Whilenode

--- SmallLang/Treewalk-Interpreter/TreeWalkInterpreter.cs
+++ SmallLang/Treewalk-Interpreter/TreeWalkInterpreter.cs
@@ -1,8 +1,10 @@
+using System.Diagnostics;
 using Common;
 using Common.Dispatchers;
 using Common.LinqExtensions;
 using Common.Tokens;
 using sly.lexer;
+using SmallLang.Exceptions;
 using SmallLang.IR.AST;
 using SmallLang.IR.AST.Generated;
 using SmallLang.IR.Metadata;
@@ -57,9 +59,63 @@ public class TreeWalkInterpreter : ISmallLangNodeVisitor<Nothing>
         throw new NotImplementedException();
     }
 
+
+    bool InterpretConditionalExpression(IExpressionNode Cond)
+    {
+
+        //TODO: Fix Bodge
+        if (Cond.GenericSLType?.OutmostType != TypeData.Bool)
+        {
+            throw new TypeErrorException(new GenericSmallLangType(TypeData.Bool), Cond.GenericSLType!, -1);
+        }
+
+        var top = State.Stack.Peek();
+
+        Interpret(Cond);
+
+        Debug.Assert(State.Stack.Peek() is Boolean boolean && ReferenceEquals(boolean, top));
+        return (bool)State.Stack.Pop().AsVal<bool>()!;
+    }
+
     public Nothing Visit(ISmallLangNode? Parent, WhileNode self)
     {
-        throw new NotImplementedException();
+
+
+        bool BreakFlag = false;
+
+        while (InterpretConditionalExpression(self.ConditionExpression))
+        {
+            try
+            {
+                Interpret(self.LoopBody);
+            }
+            catch (ControlException e)
+            {
+                if (e.TryGetDst(out var variableName) && variableName != self.LoopLabel?.Identifier.VariableName)
+                {
+                    throw;
+                }
+                else if (e.ContinueFlag)
+                {
+                    continue;
+                }
+                else if (e.BreakFlag)
+                {
+                    BreakFlag = true;
+                    break;
+                }
+                else
+                {
+                    throw new Exception("Impossible Case");
+                }
+            }
+        }
+        if (!BreakFlag)
+        {
+            Interpret(self.Else);
+        }
+
+        return Nothing.GetNothing;
     }
 
     public Nothing Visit(ISmallLangNode? Parent, LoopLabelNode self)
